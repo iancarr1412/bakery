91b712c9684832784957d462a2768ba8befb2cd7
iancarr1412 <iancarr1412@live.co.uk>
2019-02-08 20:26:31 +0000

    Initial Commit - 20190208

---
+++ Pages/Orders/Order.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Threading.Tasks;
+using Bakery.Data;
+using Bakery.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace Bakery.Pages
+{
+    public class OrderModel : PageModel
+    {
+        private BakeryContext db;
+        public OrderModel(BakeryContext db) => this.db = db;
+        [BindProperty(SupportsGet =true)]
+        public int Id { get; set; }
+        public Product Product { get; set;}
+        public async Task OnGetAsync() =>  Product = await db.Products.FindAsync(Id);
+    }
+}
