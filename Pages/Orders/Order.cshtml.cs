ebc5c0f682adb3b151a2572d90261e58c53df45c
iancarr1412 <iancarr1412@live.co.uk>
2019-02-09 19:52:10 +0000

    2nd Commit - 20190219

--- Pages/Orders/Order.cshtml.cs
+++ Pages/Orders/Order.cshtml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Bakery.Data;
 using Bakery.Models;
@@ -13,6 +14,21 @@ namespace Bakery.Pages
         [BindProperty(SupportsGet =true)]
         public int Id { get; set; }
         public Product Product { get; set;}
+        [BindProperty, EmailAddress, Required(ErrorMessage="Please enter your email address"), Display(Name="Your Email Address")]
+        public string OrderEmail { get; set; }
+        [BindProperty, Required(ErrorMessage="Please supply a shipping address"), Display(Name="Shipping Address")]
+        public string OrderShipping { get; set; } 
+        [BindProperty, Display(Name="Quantity")]
+        public int OrderQuantity { get; set; } = 1;
         public async Task OnGetAsync() =>  Product = await db.Products.FindAsync(Id);
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Product = await db.Products.FindAsync(Id);
+            if(ModelState.IsValid){
+                return RedirectToPage("Orders/OrderSuccess");
+            }
+            return Page();
+        }
     }
 }
